19f0fe35e19ecc651a85f7548d1ad934709fc1de
Bhavik <bhavik.v.patel@gmail.com>
2020-03-23 01:41:35 -0400

    Initial commit

---
+++ SQLDataToCSVConverter/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SQLDataToCSVConverter
+{
+    class Program
+    {
+        static readonly string DistinctColumnName = ConfigurationManager.AppSettings["DistinctColumnName"];
+        static void Main(string[] args)
+        {
+            ServiceProvider.LogDetails("Service Started.");
+            GenerateCSV();
+            ServiceProvider.LogDetails("Service Ended.");
+        }
+        static void GenerateCSV()
+        {
+            var columnName = DistinctColumnName;//"ApplicationObjectTypeName";
+            //"AJF_PROJ_CDE (C13)"
+            var table = ServiceProvider.ReadTable();
+            //AJF_PROJ_CDE (C13)
+            var projects = table.AsEnumerable().Select(row => row.Field<string>(columnName)).Distinct();//Select(group => group.First()).ToList();
+            foreach (var project in projects)
+            {
+                DataTable selectedTableRecords = table.AsEnumerable()
+                            .Where(r => r.Field<string>(columnName) == project)
+                            .CopyToDataTable();
+
+                ServiceProvider.LogDetails("Data Table retrieved with " + selectedTableRecords.Rows.Count + ".");
+                string exe = Process.GetCurrentProcess().MainModule.FileName;
+                string filepath = Path.GetDirectoryName(exe) + @"\Files\Report_" + project+"_" + DateTime.Now.ToString("yyyyMMddHHmm") + ".csv";
+                ServiceProvider.WriteToFile(selectedTableRecords, filepath, false, ",");
+                ServiceProvider.LogDetails("File Generated at " + filepath);
+            }
+
+        }
+    }
+}
---
+++ SQLDataToCSVConverter/ServiceProvider.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SQLDataToCSVConverter
+{
+    public static class ServiceProvider
+    {
+        // Simple example for 
+        // 1.) Read a sql server query to datatable;
+        // 2.) Export it to .csv
+        private static readonly string ConnectionString = ConfigurationManager.AppSettings["ConnectionString"];
+        private static readonly string TextDelimiter = "\"";
+        //private static readonly string SQLQuery = "Select * from Users";
+        public static DataTable ReadTable()
+        {
+            var returnValue = new DataTable();
+
+            var conn = new SqlConnection(ConnectionString);
+
+            try
+            {
+
+                var sqlQuery = ReadSQLQuery();
+                if (!string.IsNullOrEmpty(sqlQuery))
+                {
+                    conn.Open();
+                    var command = new SqlCommand(sqlQuery, conn);
+
+                    using (var adapter = new SqlDataAdapter(command))
+                    {
+                        adapter.Fill(returnValue);
+                    }
+                }
+
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                LogDetails(ex.Message + " INNER EXCEPTIONS: " + ex.InnerException);
+                //throw ex;
+            }
+            finally
+            {
+                if (conn.State == ConnectionState.Open)
+                    conn.Close();
+            }
+
+            return returnValue;
+        }
+
+        public static void WriteToFile(DataTable dataSource, string fileOutputPath, bool firstRowIsColumnHeader = false, string seperator = ";")
+        {
+            try
+            {
+                var sw = new StreamWriter(fileOutputPath, false);
+
+                int icolcount = dataSource.Columns.Count;
+
+                if (!firstRowIsColumnHeader)
+                {
+                    for (int i = 0; i < icolcount; i++)
+                    {
+                        sw.Write(TextDelimiter + dataSource.Columns[i]+ TextDelimiter);
+                        if (i < icolcount - 1)
+                            sw.Write(seperator);
+                    }
+
+                    sw.Write(sw.NewLine);
+                }
+
+                foreach (DataRow drow in dataSource.Rows)
+                {
+                    for (int i = 0; i < icolcount; i++)
+                    {
+                        if (!Convert.IsDBNull(drow[i]))
+                            sw.Write(TextDelimiter + drow[i].ToString()+ TextDelimiter);
+                        if (i < icolcount - 1)
+                            sw.Write(seperator);
+                    }
+                    sw.Write(sw.NewLine);
+                }
+                sw.Close();
+            }
+            catch (Exception ex)
+            {
+                LogDetails(ex.Message + " INNER EXCEPTIONS: " + ex.InnerException);
+            }
+        }
+        public static void LogDetails(string content)
+        {
+            string exe = Process.GetCurrentProcess().MainModule.FileName;
+            string path = Path.GetDirectoryName(exe);
+            FileStream fs = new FileStream(path + @"\Logs" + @"\ServiceLog_" + DateTime.Now.ToString("yyyyMMddHHmmss") + ".txt", FileMode.OpenOrCreate, FileAccess.Write);
+            StreamWriter sw = new StreamWriter(fs);
+            sw.BaseStream.Seek(0, SeekOrigin.End);
+            sw.WriteLine(content + " [At " + DateTime.Now.ToString("yyyyMMddHHmmss") + "]" + Environment.NewLine);
+            sw.Flush();
+            sw.Close();
+        }
+
+        private static string ReadSQLQuery()
+        {
+            string exe = Process.GetCurrentProcess().MainModule.FileName;
+            string path = Path.GetDirectoryName(exe);
+            return File.ReadAllText(path + @"\SQLQuery\SQLScript.sql");
+        }
+    }
+}
